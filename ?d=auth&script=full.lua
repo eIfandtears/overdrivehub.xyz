31b657c2aff68717e764fbc22ecf6f22a6ec4f7b
elfandtears <poo23838+alt1@gmail.com>
2025-01-25 09:06:37 -0700

    Create ?d=auth&script=full.lua

---
+++ ?d=auth&script=full.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Fizzzmy/OverdriveH/refs/heads/main/overdrivehub.lua'))()
+loadstring(game:HttpGet("https://overdrivehub.xyz/?d=auth&script=mm2_lite"))()
